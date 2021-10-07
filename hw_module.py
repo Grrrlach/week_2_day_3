3d7702c95220fa5f1da091643060a48774ef7ac8
misterglock <the75thpickle@hotmail.com>
2021-10-07 01:32:06 -0400

    first commit

---
+++ hw_module.py
@@ -0,0 +1,15 @@
+from math import pi
+
+def circ (r):
+    print (f"The circumference of your circle is {pi*r**2}")
+    
+circ(20)
+
+
+
+def sq_ft (length,width):
+    print(f"Your home's total area is: {length * width} sq. ft.")
+
+sq_ft(5,6)
+
+
